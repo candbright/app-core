c5a84a685f1cd46d5951285c70e6901e89fc5cf7
candbright <2685082823@QQ.COM>
2023-02-14 22:17:50 +0800

    修复FragmentLifecycleListener未继承IFragmentLifecycleListener的bug
    Signed-off-by: candbright <2685082823@QQ.COM>

--- app/src/main/java/com/candbright/base/fragment/FragmentLifecycleListener.java
+++ app/src/main/java/com/candbright/base/fragment/FragmentLifecycleListener.java
@@ -2,34 +2,33 @@ package com.candbright.base.fragment;
 
 import android.content.Intent;
 
-public class FragmentLifecycleListener {
-
+public class FragmentLifecycleListener implements IFragmentLifecycleListener {
+    @Override
     public void onViewCreated() {
-
     }
 
+    @Override
     public void onResume() {
-
     }
 
+    @Override
     public void onPause() {
-
     }
 
+    @Override
     public void onHiddenChanged(boolean hidden) {
-
     }
 
+    @Override
     public void onDestroyView() {
-
     }
 
+    @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-
     }
 
+    @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-
     }
 
 }
